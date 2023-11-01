18164b820fbe2fea8fb09d0bb8b8a4baa0a36bb5
a-nnza-r <ansarahmedcjc17@gmail.com>
2023-11-01 09:05:58 +0800

    updated layouting for landing page on smaller scrs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,24 @@
 "use client";
+
+import { useState } from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
 import Link from "next/link";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Drawer from "@mui/material/Drawer";
 
 export default function Header() {
-  return (
-    <Tabs sx={{ bgcolor: "#f6f6f6" }}>
-      <Tab
-        value="/"
-        label="Ansar Ahmed"
-        component={Link}
-        href="/"
-        sx={{
-          color: "#040424",
-          fontWeight: "bold",
-          fontSize: "25px",
-          fontFamily: "Copperplate",
-        }}
-      />
-      <Box
-        sx={{
-          width: "100%",
-          display: "flex",
-          justifyContent: "flex-end",
-        }}
-      >
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  function handleDrawerToggle() {
+    setMobileOpen(!mobileOpen);
+  }
+
+  function DrawerContent(props) {
+    return (
+      <Box sx={{ display: "flex", flexDirection: props.flexDirection }}>
         <Tab
           value="/education"
           label="EDUCATION"
@@ -46,6 +39,67 @@ export default function Header() {
           href="/projects"
         />
       </Box>
+    );
+  }
+
+  return (
+    <Tabs
+      sx={{
+        bgcolor: "#f6f6f6",
+        width: "100%",
+        display: "flex",
+        alignItems: "center",
+      }}
+    >
+      <Tab
+        value="/"
+        label="Ansar Ahmed"
+        component={Link}
+        href="/"
+        sx={{
+          flexGrow: 0,
+          flexShrink: 1,
+          color: "#040424",
+          fontWeight: "bold",
+          fontSize: "25px",
+          fontFamily: "Copperplate",
+        }}
+      />
+
+      {/* For larger screens */}
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: { xs: "none", sm: "none", md: "flex" },
+          justifyContent: "flex-end",
+        }}
+      >
+        <DrawerContent flexDirection={"row"} />
+      </Box>
+
+      {/* For smaller screens */}
+      <Box
+        sx={{
+          flexGrow: 1,
+          display: { xs: "flex", sm: "flex", md: "none" },
+          justifyContent: "flex-end",
+          margin: "5px",
+        }}
+      >
+        <IconButton
+          color="inherit"
+          aria-label="open drawer"
+          edge="end"
+          onClick={handleDrawerToggle}
+        >
+          <MenuIcon />
+        </IconButton>
+      </Box>
+
+      {/* Drawer for mobile view */}
+      <Drawer anchor="right" open={mobileOpen} onClose={handleDrawerToggle}>
+        <DrawerContent flexDirection={"column"} />
+      </Drawer>
     </Tabs>
   );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -4,33 +4,36 @@ import Socials from "@/components/Socials.jsx";
 
 function Profile() {
   return (
-    <div className={styles.gridLayout}>
-      <div>
+    <div className={styles.profileContainer}>
+      <div className={styles.ImgandTextConatiner}>
         <img
           src="/me.jpeg"
           alt={information.name}
           className={styles.profileImg}
         />
-      </div>
-      <div className={styles.profileTextContainer}>
-        <div>
-          <div className={styles.profileTitle}>{information.name}</div>
-          <div className={styles.profileSubtitle}>{information.occupation}</div>
 
-          <div className={styles.profileDetail}>
-            <div className={styles.profileDetailTitle}>Student Email: </div>
-            {information.studentEmail}
+        <div className={styles.profileTextContainer}>
+          <div>
+            <div className={styles.profileTitle}>{information.name}</div>
+            <div className={styles.profileSubtitle}>
+              {information.occupation}
+            </div>
           </div>
-        </div>
+          <div className={styles.detailsContainer}>
+            <div className={styles.profileDetail}>
+              <div className={styles.profileDetailTitle}>Student Email: </div>
+              {information.studentEmail}
+            </div>
 
-        <div className={styles.profileDetail}>
-          <div className={styles.profileDetailTitle}>Email: </div>
-          {information.email}
-        </div>
-
-        <div className={styles.profileDetail}>
-          <div className={styles.profileDetailTitle}>Date of Birth: </div>
-          {information.dob}
+            <div className={styles.profileDetail}>
+              <div className={styles.profileDetailTitle}>Email: </div>
+              {information.email}
+            </div>
+            <div className={styles.profileDetail}>
+              <div className={styles.profileDetailTitle}>Date of Birth: </div>
+              {information.dob}
+            </div>
+          </div>
         </div>
       </div>
 
